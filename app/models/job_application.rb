528e052ed1f6d723a7851a6ae8db711739a46f34
Nicolas Debray <nicdebray@gmail.com>
2019-05-29 14:39:24 +0200

    validation job application

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -4,5 +4,6 @@ class JobApplication < ApplicationRecord
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, format: { with: /(\A([a-z]*\s*)*\<*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\>*\Z)/i }
+  validates_uniqueness_of :job_offer_id, scope: :user_id, message: "You already applied"
 
 end
