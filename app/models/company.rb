8619a48352d7678e19adb8b64688e52f43b220f4
Nicolas Debray <nicdebray@gmail.com>
2019-05-30 21:41:06 +0200

    added geocode to company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,4 +6,6 @@ class Company < ApplicationRecord
   validates :name, :address, presence: { message: 'must be provided}' }
   validates :name, uniqueness: { case_sensitive: false }
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
