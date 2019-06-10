ec3295fac1c86961c39814d8904755bd4f186e04
Nicolas Debray <nicdebray@gmail.com>
2019-06-10 23:21:23 +0200

    made job offers available accros the whole application (for the dd menu link)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :make_company_dispo
+  before_action :make_job_offers_dispo
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
@@ -25,6 +26,10 @@ class ApplicationController < ActionController::Base
 
   def make_company_dispo
     @companies ||= Company.all
+  end
+
+  def make_job_offers_dispo
+    @my_job_offers ||= JobOffer.joins(:company).where(companies: {user_id: current_user}).where('start_date > ?', DateTime.now)
   end
 
   def skip_pundit?
